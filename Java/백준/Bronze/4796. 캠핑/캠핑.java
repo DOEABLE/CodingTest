8578c2d8b64e1f7c9f062598a2e79af6e5252c7b
KIM DOHEE <111475042+DOEABLE@users.noreply.github.com>
2025-03-07 20:54:12 +0900

    [Bronze I] Title: 캠핑, Time: 140 ms, Memory: 15948 KB -BaekjoonHub

--- Java/백준/Bronze/4796. 캠핑/캠핑.java
+++ Java/백준/Bronze/4796. 캠핑/캠핑.java
@@ -1,1 +1,34 @@
-import java.io.BufferedWriter;import java.io.BufferedReader;import java.io.InputStreamReader;import java.io.OutputStreamWriter;import java.util.Arrays;import java.util.StringTokenizer; public class Main {    public static void main(String[] args) throws Exception {        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));                int idx = 1;        while(true) {            StringTokenizer st = new StringTokenizer(br.readLine(), " ");            int L = Integer.parseInt(st.nextToken());            int P = Integer.parseInt(st.nextToken());            int V = Integer.parseInt(st.nextToken());                        if (L == 0) {                break;            }                        int useDay = V / P * L + Math.min(L, V % P);                        bw.write("Case " + idx++ + ": " + useDay + "\n");        }         bw.flush();        bw.close();        br.close();    }}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int idx = 1;
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			int L = Integer.parseInt(st.nextToken());
+			int P = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+
+			if (L == 0) {
+				break;
+			}
+
+			int useDay = V / P * L + Math.min(L, V % P);
+			bw.write("Case " + idx++ + ": " + useDay + "\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
