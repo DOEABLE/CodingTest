a75c25d1331e08632eac9e9fba1c4c48e92187b4
KIM DOHEE <111475042+DOEABLE@users.noreply.github.com>
2025-04-29 10:50:45 +0900

    [Silver I] Title: 절댓값 힙, Time: 272 ms, Memory: 25276 KB -BaekjoonHub

--- Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,42 +1,45 @@
-import java.util.*;
-import java.io.*;
-public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer a, Integer b){
-                int A = Math.abs(a);
-                int B = Math.abs(b);
-                
-                if(A>B)
-                    return A-B;
-                else if(A==B){
-                    if(a>b) return 1;
-                    else return -1;
-                }
-                else return -1;
-            }
-        });
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        while(N-->0){
-            int num = Integer.parseInt(br.readLine());
-            
-            if(num !=0){
-                pq.offer(num);
-            }else{
-                if(!pq.isEmpty()){
-                    sb.append(pq.poll()).append("\n");
-                }else
-                    sb.append("0\n");
-            }
-        }
-        System.out.println(sb);
-        
-        
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				//절댓값 기준으로 앞이 더 크면 자리 바꿈
+				if (Math.abs(o1) > Math.abs(o2)) {
+					return Math.abs(o1) - Math.abs(o2);
+				}
+				//절댓값 기준 두 값이 같다면 음수를 앞으로 보내줌
+				else if (Math.abs(o1) == Math.abs(o2)) {
+					return o1 - o2;
+				} else
+					return -1;
+
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if (pq.isEmpty())
+					sb.append("0").append("\n");
+				else
+					sb.append(pq.poll()).append("\n");
+			} else {
+				pq.offer(x);
+			}
+		}
+		System.out.println(sb);
+	}
+
 }
