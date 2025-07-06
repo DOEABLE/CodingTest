4a6c342cc35b9c73d8389d73de8a0f3249735cb6
KIM DOHEE <111475042+DOEABLE@users.noreply.github.com>
2025-07-06 23:24:32 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 308 ms, Memory: 18480 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (j < N - i)
+					System.out.print(" ");
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
